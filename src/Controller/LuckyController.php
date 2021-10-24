48d047921e3effed3bbbd4082d0ea856d3176490
Mr Ilyes <ilyesghlila1@gmail.com>
2021-10-24 19:07:39 +0100

    commit

---
+++ src/Controller/LuckyController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LuckyController extends AbstractController
+{
+    /**
+     * @Route("/lucky", name="lucky")
+     */
+    public function index(): Response
+    {
+        return $this->render('lucky/index.html.twig', [
+            'controller_name' => 'LuckyController',
+        ]);
+    }
+}
