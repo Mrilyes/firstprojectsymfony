3b0b4edeac195aa08ac8b2378d5fe992148c4019
Mr Ilyes <ilyesghlila1@gmail.com>
2021-11-12 13:26:04 +0100

    commit

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -5,33 +5,41 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class LuckyController extends AbstractController
 {
    
      /**
-     * @Route("/lucky/{max}", name="lucky")
+     * @Route("/lucky/{max?}", name="lucky")
      */
 
 
-    public function number2(int $max): Response
+    public function number2($max): Response
     {
-
+        if(!isset($max)||(!is_numeric($max) )) {
+            throw new HttpException(404 , "il faut entrer un numero pas un caractere");
+            }
+       
         $ma = random_int(0, $max);
         
         return $this->render('lucky/number.html.twig', [
-            'ma' => $ma,
+            'ma' => $max,
         ]);
+
+       
     }
 
 
 
-    // public function index(): Response
-    // {
+    // public function index2(): Response
+
+     
+    
     //     return $this->render('lucky/index.html.twig', [
     //         'number' => $number,
     //     ]);
-    // }
+ 
 
     
     
